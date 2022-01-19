321ed20646f352b453bdbe344697efb5dc2799cb
umaishussain <94458046+umaishussain@users.noreply.github.com>
2022-01-19 01:27:25 -0800

    Add files via upload

---
+++ 4assingment.dart
@@ -0,0 +1,61 @@
+//question_1
+// import 'dart:io';
+
+// void main(){
+//  List d = ["syed", "umais","hussain"];
+//  print("${d}");
+// }
+//question_2
+//void main(){
+ // List data = [12,45.67,"umais", false];
+ // print(data.runtimeType);
+//}
+//question_3
+// void main(){
+//   List e = ["SSC","HSC","BSC","BS","BCOM","MS","M.Phil","PhD"];
+//   print("${e[0]}, ${e[1]}, ${e[2]}, ${e[3]}, ${e[4]}, ${e[5]}, ${e[6]}, ${e[7]}",);
+//}
+// //question_4
+// void main (){
+//   List data = ["zarar","umais","athar"];
+//   List data2 = [450,444,438];
+//   print("Student Name:" "${data[0]}");
+//   print("student markes:" "${data2[0]}");
+//   print("per% : " "${data2[0]/500*100}");
+
+//   print("Student Name:" "${data[1]}");
+//   print("student markes:" "${data2[1]}");
+//   print("per% : " "${data2[1]/500*100}");
+
+//   print("Student Name:" "${data[2]}");
+//   print("student markes:" "${data2[2]}");
+//   print("per% : " "${data2[2]/500*100}");
+// }
+
+//question_05
+
+// void main(){
+//   List score = [87,90,76,98,66,48];
+//   score.sort();
+//   print(score);
+// }
+
+//question_06
+
+// void main(){
+//   List score = [67,65,87,95,55,99];
+//   score.sort();
+//   var b = List.of(score.reversed);
+//   print(b);
+// }
+
+//question_07
+
+// void main (){
+//   List data = ["umais", 45 , 39 ,"zarar","true"];
+//   if (data != null){
+//     print("List is present");
+//     data.replaceRange(2, 3, ["changes"]);
+//     print(data);
+//   }
+// }
